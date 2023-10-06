0ed114d3bcfbc182ad95d670bb46287180b01c17
mansour <mansourbenahmed89@gmail.com>
2023-10-06 16:11:21 +0100

    setup server and web socket

---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require("express")
+const cors= require("cors")
+const app=express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+const server =require("http").createServer(app)
+const PORT=5000
+const io=require("socket.io")(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"]
+    }
+})
+
+server.listen(PORT,()=>{
+    console.log("lestning port "+PORT)
+})
